7742705ff2c89418d45084ac8b6aac0f32502fcb
Carlos Henrique <carlossantosdm@gmail.com>
2017-11-29 11:12:38 -0200

    correcao exemplo edicao de ura

--- exemplos/editarUra.php
+++ exemplos/editarUra.php
@@ -5,37 +5,34 @@ use TotalVoice\Client as TotalVoiceClient;
 
 $client = new TotalVoiceClient('SEU_TOKEN');
 
-
 $id = "10";
 $nomeUra = "Ura Diurna";
 $dados = json_decode('
-    [{
-        "acao":"tts",
-        "opcao":"", 
-        "menu": "menu 1", 
-        "acao_dados":{
-            "mensagem": "Olá, bem vindo a totalvoice, digite 1 para suporte, 2 para financeiro"
-        }
-    },
-    {
-        "acao":"transfer",
-        "opcao":"1", 
-        "menu": "menu 1", 
-        "acao_dados":{
-            "numero_telefone": "4000"
-        }
-    },
-    {
-        "acao":"transfer",
-        "opcao":"2", 
-        "menu": "menu 1", 
-        "acao_dados":{
-            "numero_telefone": "4010"
-        }
-    }]'
+[{
+    "acao":"tts",
+    "opcao":"", 
+    "menu": "menu 1", 
+    "acao_dados":{
+        "mensagem": "Olá, bem vindo a totalvoice, digite 1 para suporte, 2 para financeiro"
+    }
+},
+{
+    "acao":"transferir",
+    "opcao":"1", 
+    "menu": "menu 1", 
+    "acao_dados":{
+        "numero_telefone": "4000"
+    }
+},
+{
+    "acao":"transferir",
+    "opcao":"2", 
+    "menu": "menu 1", 
+    "acao_dados":{
+        "numero_telefone": "4010"
+    }
+}]'
 );
-
-
 
 $response = $client->central->atualizarUra($id, $nomeUra, $dados);
 echo $response->getContent();
