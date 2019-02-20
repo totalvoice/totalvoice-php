f5bdc1dcbf0573171a04003b45ec37c1a8f6d846
Dilo Wagner <diegowagner4@gmail.com>
2019-02-20 14:33:04 -0300

    Adicionado método para verificar informações de pausa do Ramal

--- src/Api/Central.php
+++ src/Api/Central.php
@@ -67,6 +67,25 @@ class Central extends ApiRelatorioChamadas
     }
 
     /**
+     * Recupera as informações de pausas do ramal
+     * @param int $id
+     * @param \DateTime $dataInicio
+     * @param \DateTime $dataFinal
+     * @return mixed
+     */
+    public function relatorioPausasRamal($id, \DateTime $dataInicio, \DateTime $dataFinal)
+    {
+        $dataInicio->setTimezone(new \DateTimeZone('UTC'));
+        $dataFinal->setTimezone(new \DateTimeZone('UTC'));
+        return $this->client->get(
+            new Route([self::ROTA_RAMAL, $id, '/pausas']), [
+                'data_inicio' => $dataInicio->format('Y-m-d H:i:s e'),
+                'data_fim'    => $dataFinal->format('Y-m-d H:i:s e')
+            ]
+        );
+    }
+
+    /**
      * Requisita a URL do webphone de um ramal
      * @param array $data
      * @return mixed
